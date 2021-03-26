563279a3928b9c060978dae214fbe39c9910f0ac
tomxscode <tomasbarrosg13@gmail.com>
2021-03-26 18:38:09 -0300

    función: addUser

---
+++ app.js
@@ -0,0 +1,40 @@
+// dom
+const
+  // buttons
+  addUser = document.querySelector('#addUser'),
+  doubleMoney = document.querySelector('#doubleMoney'),
+  showMillonaires = document.querySelector('#showMillonaires'),
+  sortByRichest = document.querySelector('#sortByRichest'),
+  calculateWealth = document.querySelector('#calculateWealth'),
+  // elements
+  usersTable = document.querySelector('#usersTable')
+  ;
+
+var users = ['Maksi Allgig'];
+var wealth = [300000];
+
+// addUser function
+addUser.onclick = function() {
+  let names = ['John', 'Steven', 'Thomas', 'Elon'];
+  let lastNames = ['Doe', 'Universe', 'Edinson', 'Musk'];
+  // add user
+  let generatedName = names[Math.floor(Math.random() * names.length)] + ' ' + lastNames[Math.floor(Math.random() * lastNames.length)];
+  users.push(generatedName);
+  wealth.push(Math.floor(Math.random() * 999999));
+  // add user to dom
+  let tr = document.createElement('tr');
+  let userName = document.createElement('td');
+  let userWealth = document.createElement('td');
+  // add text to elements
+  userName.innerHTML = generatedName;
+  userWealth.innerHTML = wealth[wealth.length - 1];
+  // add elements to html
+  tr.appendChild(userName);
+  tr.appendChild(userWealth);
+  usersTable.appendChild(tr);
+
+  console.info(users, wealth);
+}
+
+// doubleMoney function
+doubleMoney
